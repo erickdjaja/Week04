4d317deb993ed0f69d84012ff1a011b845708937
erickdjaja <erick.djaja@gmail.com>
2025-09-17 11:59:30 +0700

    initial commit

---
+++ src/week04/week04.java
@@ -0,0 +1,14 @@
+package week04;
+
+public class week04 {
+
+	public week04() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
